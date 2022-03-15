7c98c1a2e67afe32fa322f2a66c7fdcde1bb80a7
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-03-15 19:00:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,57 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+        cout << arr[i] << ' ' << arr[j] << endl;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = len - 1; j > i; j--) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+      if (arr[i] + arr[j] < value) break;
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int end = len - 1;
+  for (int i = 0; i < len - 1; i++) {
+    bool result = false;
+    int start = i+1;
+    int mid = (start + end) / 2 ;
+    while (start <= end) {
+      if (arr[mid] == value - arr[i]) {
+        count++;
+        result = true;
+        break;
+      } else if (arr[mid] > value - arr[i]){
+        end = mid - 1;
+      } else if (arr[mid] < value - arr[i]) start = mid + 1 ;
+      mid = (start + end) / 2;
+    }
+    int temp1 = mid;
+    int temp2 = mid;
+    if (result == true) {
+      while (arr[temp1-1] == arr[mid]) {
+        count++;
+        temp1--;
+      }
+      while (arr[temp2+1] == arr[mid]) {
+        count++;
+        temp2++;
+      }
+    }
+  }
+  return count;
 }

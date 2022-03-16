7e9194bb0e24597b8e9fefa369c24a13d3bbb682
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-03-16 12:07:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,6 +11,7 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
+    if (arr[i] > value / 2) break;
     for (int j = len - 1; j > i; j--) {
       if (arr[i] + arr[j] == value) count++;
       if (arr[i] + arr[j] < value) break;
